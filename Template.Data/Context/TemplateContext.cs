62ab9e7c44e398678624312e1094a3ae8a9bc686
Gubio Garcia <gubiogarcia@gmail.com>
2024-07-12 23:06:19 -0300

    feat: Configure User entity mapping

---
+++ Template.Data/Context/TemplateContext.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Template.Data.Mappings;
+using Template.Domain.Entities;
+
+namespace Template.Data.Context
+{
+    public class TemplateContext: DbContext
+    {
+        public TemplateContext(DbContextOptions<TemplateContext> option)
+            : base(option) { }
+
+        #region
+
+        public DbSet<User> Users { get; set; }
+
+        #endregion
+
+        // sobrescreve o método de mesma assinatura na classe DbContext
+        // recebe parâmetro do tipo "ModelBuilder", usado para construi o modelo de dados
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            // ApplyConfiguration: aplica a configuração especificada pela classe UserMap ao modelo de dados
+            modelBuilder.ApplyConfiguration(new UserMap());
+
+            // garante que as configurações padrão da classe base sejam aplicadas juntamente com as configurações personalizadas
+            base.OnModelCreating(modelBuilder);
+        }
+    }
+}
---
+++ Template.Data/Mappings/UserMap.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Template.Domain.Entities;
+
+namespace Template.Data.Mappings
+{
+    // define a configuração de mapeamento para a entidade User para a tabela do DB
+    public class UserMap: IEntityTypeConfiguration<User>
+    {
+
+        //  Configure: método obrigátorio
+        //  recebe objeto EntityTypeBuilder<User> que permite configurar propriedades da entidade User
+        public void Configure(EntityTypeBuilder<User> builder)
+        {
+            builder.Property(x => x.Id).IsRequired();
+
+            builder.Property(x => x.Name).HasMaxLength(100).IsRequired();
+        }
+    }
+}
