7e9b4c8b1adbf9c6efdaeb4512442586049135ec
Gubio Garcia <gubiogarcia@gmail.com>
2024-07-13 20:15:31 -0300

    feat: Configure ConnectionString and DbContext

--- Template/Program.cs
+++ Template/Program.cs
@@ -1,7 +1,17 @@
+using Microsoft.EntityFrameworkCore;
+using Template.Data.Context;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
+
+// disponibiliza o TemplateContext como conteiner de injeção de dependência
+// disponibiliza a ser injetado em outras partes do projeto
+builder.Services.AddDbContext<TemplateContext>(options => options.UseSqlServer(
+    // puxa a string de conexao do appsettings.json    
+    builder.Configuration.GetConnectionString("TemplateDB")
+).EnableSensitiveDataLogging()); // ativa dados da app a serem incluídos nas mensagens de exceção
 
 var app = builder.Build();
 
