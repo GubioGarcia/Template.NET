73756a2541eb647425fdb432c26671020b3fdd1b
Gubio Garcia <gubiogarcia@gmail.com>
2024-07-13 23:28:08 -0300

    feat: Controller/Services Relationship Definition *with error*

---
+++ Template.Application/Services/UserService.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Template.Application.Interfaces;
+
+namespace Template.Application.Services
+{
+    internal class UserService: IUserService
+    {
+        public void Test()
+        {
+
+        }
+    }
+}
