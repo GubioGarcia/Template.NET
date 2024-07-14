9b3342712f28ab5ceb360da830e4ef7275060eeb
Gubio Garcia <gubiogarcia@gmail.com>
2024-07-14 14:25:18 -0300

    feat: communication Services and API Get

--- Template.Application/Services/UserService.cs
+++ Template.Application/Services/UserService.cs
@@ -5,14 +5,31 @@ using System.Text;
 using System.Threading.Tasks;
 using Template.Application.Interfaces;
 using Template.Application.ViewModels;
+using Template.Domain.Entities;
+using Template.Domain.Interfaces;
 
 namespace Template.Application.Services
 {
     public class UserService: IUserService
     {
+
+        private readonly IUserRepository userRepository;
+
+        public UserService(IUserRepository userRepository)
+        {
+            this.userRepository = userRepository;
+        }
+
         public List<UserViewModel> Get()
         {
-            return null;
+            List<UserViewModel> _userViewModels = new List<UserViewModel>();
+
+            IEnumerable<User> _users = this.userRepository.GetAll();
+            
+            foreach (var item in _users)
+                _userViewModels.Add(new UserViewModel { Id = item.Id, Email = item.Email, Name = item.Name});
+
+            return _userViewModels;
         }
     }
 }
