9d2a0152aec12df21aad9c080327148ac7d42195
Gubio Garcia <gubiogarcia@gmail.com>
2024-07-18 22:33:58 -0300

    feat: Defined private and public routes

--- Template/Controllers/UsersController.cs
+++ Template/Controllers/UsersController.cs
@@ -1,13 +1,17 @@
-﻿using Microsoft.AspNetCore.Http;
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.IdentityModel.Tokens;
 using System.Runtime.CompilerServices;
+using System.Security.Claims;
 using Template.Application.Interfaces;
 using Template.Application.ViewModels;
+using Template.Auth.Services;
 
 namespace Template.Controllers
 {
     [Route("api/[controller]")]
-    [ApiController]
+    [ApiController, Authorize]
     public class UsersController : ControllerBase
     {
 
@@ -24,7 +28,7 @@ namespace Template.Controllers
             return Ok(this.userService.Get());
         }
 
-        [HttpPost]
+        [HttpPost, AllowAnonymous]
         public IActionResult Post(UserViewModel userViewModel)
         {
             return Ok(this.userService.Post(userViewModel));
@@ -42,13 +46,17 @@ namespace Template.Controllers
             return Ok(this.userService.Put(userViewModel));
         }
 
-        [HttpDelete("{id}")]
-        public IActionResult Delete(string id)
+        [HttpDelete]
+        public IActionResult Delete()
         {
-            return Ok(this.userService.Delete(id));
+            // HttpContext.User.Identity obtém a identidade do usuário autenticado
+            // TokenService.GetValueFromClaim busca um valor específico, neste caso o ClaimTypes.NameIdentifier
+            string _userId = TokenService.GetValueFromClaim(HttpContext.User.Identity, ClaimTypes.NameIdentifier);
+
+            return Ok(this.userService.Delete(_userId));
         }
 
-        [HttpPost("authenticate")]
+        [HttpPost("authenticate"), AllowAnonymous]
         public IActionResult Authenticate(UserAuthenticateRequestViewModel userViewModel)
         {
             return Ok(this.userService.Authenticate(userViewModel));
